971af72c6650b08729efc382cb49d75d0cb0bdba
Mark Peters <mapeters101@earthlink.net>
2025-02-19 13:08:18 -0500

    Work on lesson024 to 6:00

---
+++ lesson024.py
@@ -0,0 +1,35 @@
+
+a = 100
+print(a)
+print(a+11)
+b = a + 11
+print(b)
+#print(10 = 10)
+#print(b = 10)
+
+print(10 == 10)
+a = 3.14
+print(a)
+print('-'*30)
+
+test = 100
+test_1 = 10
+_test_1_ = 10
+__test__ = 10
+TEST = 10
+print(test, TEST)
+#1_test = 10
+#if = 10
+a = float(10)
+print(a)
+
+float = 100.5
+print(float)
+#a = float(10)
+del float
+a = float(9)
+print(a)
+print('-'*30)
+
+
+

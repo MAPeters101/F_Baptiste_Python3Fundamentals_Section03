4d38628ef0cdde1a1ce64a2994fc04832370fc9f
Mark Peters <mapeters101@earthlink.net>
2025-02-19 17:13:26 -0500

    Work on lesson030 to 6:00.

---
+++ lesson030.py
@@ -0,0 +1,25 @@
+a = 10
+b = 3
+print(a/b)
+print(a//b)
+print()
+print(-12/5)
+print(-12//5)
+print()
+print(10%3)
+print(4%2)
+print(5%2)
+print(13567%2)
+print()
+print(165/60)
+print(165 - (2*60))
+print(165//60)
+print(165- (165//60 * 60))
+print(165%60)
+print()
+elapsed_minutes = 165
+hours = elapsed_minutes // 60
+remaining_minutes = elapsed_minutes % 60
+print(f"{elapsed_minutes} minutes = {hours} hours and {remaining_minutes} minutes.")
+
+

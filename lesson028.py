8d4b9550baf6bb4270848295f09d95cf58532035
Mark Peters <mapeters101@earthlink.net>
2025-02-19 16:24:40 -0500

    Complete work on lesson028 from 3:50.

--- lesson028.py
+++ lesson028.py
@@ -12,6 +12,24 @@ print(10 * 2 ** 3)
 print(10 * (2 ** 3))
 print('-'*30)
 
+principal = 100
+apr = 0.1
+years = 10
 
+future_value = principal * ((1 + apr/12) ** (years * 12))
+print(future_value)
 
+future_value = principal * (1 + apr/12) ** (years * 12)
+print(future_value)
+print('-'*30)
 
+print(2 ** -8)
+print(2 ** (-8))
+
+print(-4 ** 0.5)
+print((-4) ** 0.5)
+
+n = 10
+print(n * n - 1)
+n = 10
+print(n * (n - 1))

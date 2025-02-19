579d8b6b8271f8c453be2777ce5b53dbf4356d61
Mark Peters <mapeters101@earthlink.net>
2025-02-19 00:38:30 -0500

    Complete work on lesson020 from 4:00.

--- lesson020.py
+++ lesson020.py
@@ -9,7 +9,13 @@ print('-'*30)
 print(0.1)
 print(format(0.1, '.25f'))
 print(format(0.125, '.25f'))
+print('-'*30)
 
-
-
+print(1 + 1 + 1 == 3)
+print(0.125 + 0.125 + 0.125 == 0.375)
+print(0.1 + 0.1 + 0.1 == 0.3)
+print(format(0.1 + 0.1 + 0.1, '.25f'))
+print(format(0.3, '.25f'))
+print(abs(0.1 + 0.1 + 0.1) - 0.3)
+print((abs(0.1 + 0.1 + 0.1) - 0.3) < 0.001)
 

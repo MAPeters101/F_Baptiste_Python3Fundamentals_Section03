682655df8f7f43d17d60bbb6f433f8784b97c910
Mark Peters <mapeters101@earthlink.net>
2025-02-19 17:17:57 -0500

    Complete work on lesson030 from 6:00.

--- lesson030.py
+++ lesson030.py
@@ -22,4 +22,29 @@ hours = elapsed_minutes // 60
 remaining_minutes = elapsed_minutes % 60
 print(f"{elapsed_minutes} minutes = {hours} hours and {remaining_minutes} minutes.")
 
+elapsed_minutes = 485
+hours = elapsed_minutes // 60
+remaining_minutes = elapsed_minutes % 60
+print(f"{elapsed_minutes} minutes = {hours} hours and {remaining_minutes} minutes.")
+
+elapsed_minutes = 10485
+hours = elapsed_minutes // 60
+remaining_minutes = elapsed_minutes % 60
+print(f"{elapsed_minutes} minutes = {hours} hours and {remaining_minutes} minutes.")
+
+print('-'*80)
+
+total = 0
+for i in range(1, 1_001):
+    total += i
+    print(f'total = {total}...')
+print(f'Final total = {total}...')
+print()
+
+total = 0
+for i in range(1, 1_001):
+    total += i
+    if i%100 == 0:
+        print(f'total = {total}...')
+print(f'Final total = {total}...')
 

adc93da20211d246855827455b9760a76d91531d
Mark Peters <mapeters101@earthlink.net>
2025-02-19 23:27:47 -0500

    Description for all exercise02.

---
+++ exercise02.py
@@ -0,0 +1,9 @@
+"""
+Exercise 2
+Assume you have some variable elapsed that contains elapsed time in seconds.
+
+Create three new variables: hours, minutes and seconds, that represent the n
+umber of hours, minutes and seconds represented by elapsed.
+
+For example, if elapsed = 7835, then hours = 2, minutes = 10 and seconds = 35
+"""

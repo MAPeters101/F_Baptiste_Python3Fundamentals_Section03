39554eec85282b9eae4e5f8e7e7c9b9e32a5cd3e
Mark Peters <mapeters101@earthlink.net>
2025-02-20 00:04:28 -0500

    Work on lesson032 from 6:15 to 9:30.

--- lesson032.py
+++ lesson032.py
@@ -33,3 +33,18 @@ print(10 != 12)
 print(10.5 != 12.5)
 print(10 >= 5)
 print(10.5 < 100)
+print()
+
+a = 1 + 1j
+b = 1 + 1j
+c = 2 + 2j
+print(a == b)
+print(a is b, id(a), id(b))
+#print(a < c)
+print()
+
+print(0.1*3 == 0.3)
+
+
+
+

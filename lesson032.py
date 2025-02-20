51dbd52f8011f32739e0cb8c12caed391ef85d08
Mark Peters <mapeters101@earthlink.net>
2025-02-19 23:57:53 -0500

    Work on lesson032 to 5:00.

---
+++ lesson032.py
@@ -0,0 +1,32 @@
+a = 10
+b = 10
+
+print(a == b)
+
+c = 10.0
+print(a == c)
+print(a is c)
+print()
+
+a = 10
+b = 10.0
+print(a == b)
+print(a is b)
+print(id(a), id(b))
+print()
+
+a = 10
+b = 10
+print(a == b)
+print(a is b)
+print(id(a), id(b))
+print()
+
+a = 10_000
+b = 10_000
+print(a == b)
+print(a is b)
+print(id(a), id(b))
+print()
+
+

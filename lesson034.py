2cb94cec95d635a48bc46b75f168debdb26622b8
Mark Peters <mapeters101@earthlink.net>
2025-02-20 15:23:53 -0500

    Work on lesson034 from 2:10 to 3:30.

--- lesson034.py
+++ lesson034.py
@@ -17,3 +17,23 @@ print()
 print('-'*80)
 
 
+balance = 1000.00
+withdrawal = 50.00
+withdrawal_limit = 500.00
+print((withdrawal < withdrawal_limit) and (withdrawal <= balance))
+print()
+
+balance = 1000.00
+withdrawal = 600.00
+withdrawal_limit = 500.00
+print((withdrawal < withdrawal_limit) and (withdrawal <= balance))
+print()
+
+balance = 1000.00
+withdrawal = 1200.00
+withdrawal_limit = 1500.00
+print((withdrawal < withdrawal_limit) and (withdrawal <= balance))
+print()
+print('-'*80)
+
+

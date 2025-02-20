814bddfdee5b43134430a79d260acdc4c9c3d7c3
Mark Peters <mapeters101@earthlink.net>
2025-02-20 15:19:17 -0500

    Work on lesson034 to 2:10

---
+++ lesson034.py
@@ -0,0 +1,19 @@
+
+print(not True)
+print(not False)
+print()
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print()
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+print()
+print('-'*80)
+
+

5df3691263a81fe88b5900919ad40b682f8ce288
Mark Peters <mapeters101@earthlink.net>
2025-02-20 15:33:03 -0500

    Complete exercise01.

--- exercise01.py
+++ exercise01.py
@@ -4,3 +4,14 @@ Given two floats a and b, and some tolerance tol, write an expression that
 will test whether a and b are within tol of each other.
 """
 
+a = 0.1
+b = 0.11
+tol = .009
+print(abs(a-b) < tol)
+
+a = 0.1
+b = 0.11
+tol = .012
+print(abs(a-b) < tol)
+
+
